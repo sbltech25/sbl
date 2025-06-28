2ccaf8286b9e818d4112c5c9e2ba568b24912b43
justphemi <justphemi@gmail.com>
2025-06-28 05:20:54 +0100

    undo cors

--- backend/index.js
+++ backend/index.js
@@ -21,13 +21,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
